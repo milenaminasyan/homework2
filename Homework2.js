f4e7e0091d1757618b5aed06d61e1c9c7ba24883
Milena <milena_minasyan@edu.aua.am>
2018-09-10 19:14:06 +0400

    first commit

---
+++ Homework2.js
@@ -0,0 +1,27 @@
+'homework2'
+
+<script>
+
+const a = 7;
+const b = (a + 8);
+const c = 1;
+const f = ('Joe');
+const y = ((a - b) * c);
+const z = ((a * c) - (b + c + a));
+
+
+if(z = (a - y) && y = c || f === y)  {
+console.log('Hello');
+} else {
+console.log('NO');
+}
+if(((b - 2a) === c) || (b === c)) {
+	console.log('Yes');
+} else {
+	console.log('No Way');
+}
+
+</script>
+
+
+
